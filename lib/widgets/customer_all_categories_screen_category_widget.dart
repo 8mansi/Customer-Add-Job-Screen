a31b736a48760b4782ec8f7ea146ce463d18c9eb
MelittaPacheco <melittapacheco@gmail.com>
2023-03-27 20:38:16 +0530

    all categories with name and image widget

--- lib/widgets/customer_all_categories_screen_category_widget.dart
+++ lib/widgets/customer_all_categories_screen_category_widget.dart
@@ -14,6 +14,32 @@ class _CustomerAllDetailScreenCategoryWidgetState
     extends State<CustomerAllDetailScreenCategoryWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(children: [
+                Image.network(
+                  widget.skillImageUrl,
+                  height: 60,
+                  width: 60,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  widget.skillName,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
+                ),
+              ]),
+            ),
+          ],
+        ));
   }
 }
