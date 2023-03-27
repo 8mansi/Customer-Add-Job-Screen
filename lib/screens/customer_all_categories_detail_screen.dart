9e301b0e03041f511b014f3b83bc36369a158247
MelittaPacheco <melittapacheco@gmail.com>
2023-03-27 20:32:59 +0530

    Added all categories details screen

--- lib/screens/customer_all_categories_detail_screen.dart
+++ lib/screens/customer_all_categories_detail_screen.dart
@@ -20,7 +20,7 @@ class _CustomerAllCategoriesDetailScreenState
       "profileImageUrl":
           "https://res.cloudinary.com/dyouf5py6/image/upload/v1675765220/ravinaik%40gmail.com.jpg",
       "address": "Margao",
-      'rating': '5',
+      'rating': 5,
       'totalNumberOfRatings': '19'
     },
     {
@@ -29,7 +29,7 @@ class _CustomerAllCategoriesDetailScreenState
       "profileImageUrl":
           "https://res.cloudinary.com/dyouf5py6/image/upload/v1675767993/kimvaz%40gmail.com.jpg",
       "address": "Panjim",
-      'rating': '4',
+      'rating': 4,
       'totalNumberOfRatings': '15'
     },
     {
@@ -38,16 +38,82 @@ class _CustomerAllCategoriesDetailScreenState
       "profileImageUrl":
           "https://res.cloudinary.com/dyouf5py6/image/upload/v1676281621/cruzdias%40gmail.com.jpg",
       "address": "Vasco",
-      'rating': '3',
+      'rating': 3,
       'totalNumberOfRatings': '25'
     }
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("CUSTOMER ALL CATEGORIES DETAIL SCREEN"),
-      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(backgroundColor: Theme.of(context).primaryColor),
+      body: Column(children: [
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          color: Colors.white,
+          margin: EdgeInsets.all(10),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              suffixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Mason',
+          style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 24,
+              color: Theme.of(context).primaryColor),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Building strong foundations for a better tomorrow',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).primaryColor),
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return InkWell(
+                // onTap: () => {
+                //   Navigator.pushNamed(
+                //     context,
+                //     CustomerAllCategoriesScreen.routeName,
+                //     arguments: testimonials[index].id,
+                //   )
+                // },
+                child: CustomerAllCategoriesDetailScreenServiceProviderWidget(
+                  // _id: testimonials[index]._id,
+                  fullName: testimonials[index]['fullName'],
+                  profileImageUrl: testimonials[index]['profileImageUrl'],
+                  address: testimonials[index]['address'],
+                  rating: testimonials[index]['rating'],
+                  totalNumberOfRatings: testimonials[index]
+                      ['totalNumberOfRatings'],
+                ),
+              );
+            },
+            itemCount: testimonials.length,
+          ),
+        ),
+      ]),
     );
   }
 }
